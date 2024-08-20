d4492790048a73f34ee9b9ba142408d0066ad00c
yanggw <yanggw@glodon.com>
2024-08-20 19:04:00 +0800

    add nav project

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: '博客' },
   { href: '/tags', title: '标签' },
   { href: '/projects', title: '作品' },
+  { href: 'https://nav.guowei.fun/', title: '导航' },
   { href: '/about', title: '关于' },
 ]
 
