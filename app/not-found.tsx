edcfa0330edb705f6ae49883e598aa482e8641c4
yanggw <yanggw@glodon.com>
2024-08-28 11:35:32 +0800

    i18n

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -10,14 +10,14 @@ export default function NotFound() {
       </div>
       <div className="max-w-md">
         <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-          Sorry we couldn't find this page.
+          抱歉，我们找不到此页面。
         </p>
-        <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
+        <p className="mb-8">但不用担心，您可以在我们的主页上找到很多其他东西。</p>
         <Link
           href="/"
           className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500"
         >
-          Back to homepage
+          返回主页
         </Link>
       </div>
     </div>
