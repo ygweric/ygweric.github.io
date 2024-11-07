596481f13e099f7aee4511ad122ee7d32b9810d8
国wei <ygweric@gmail.com>
2024-11-07 16:47:55 +0800

    add google analytics

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -41,9 +41,9 @@ const siteMetadata = {
     // posthogAnalytics: {
     //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     // },
-    // googleAnalytics: {
-    //   googleAnalyticsId: '', // e.g. G-XXXXXXX
-    // },
+    googleAnalytics: {
+      googleAnalyticsId: 'G-Q6SLT07SY2', // e.g. G-XXXXXXX
+    },
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
